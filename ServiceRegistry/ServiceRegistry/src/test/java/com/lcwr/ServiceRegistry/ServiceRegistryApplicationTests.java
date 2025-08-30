4ed20d59180247f4820df5c59be70c8b560f9814
rrshabh9483-cpu <rrshabh9483@gmail.com>
2025-08-30 09:22:38 +0530

    adding unresolved files

---
+++ ServiceRegistry/ServiceRegistry/src/test/java/com/lcwr/ServiceRegistry/ServiceRegistryApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lcwr.ServiceRegistry;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ServiceRegistryApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
